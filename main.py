f65fbe838294c5635f129a52e2de9993b47dc728
Suge <sugesuj@gmail.com>
2024-03-28 23:25:07 +0000

    feat: messages api

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import os
 
 import firebase_admin
-from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2AuthorizationCodeBearer
 from firebase_admin import auth, exceptions, firestore
 from openai import OpenAI
 
+from routes.messages_route import router as messages_router
 from routes.modules_route import router as modules_router
 from routes.tutors_route import router as tutors_router
-from schemas import (MessageRequest, MessageResponse, UserRegistrationRequest,
-                     UserRegistrationResponse)
+from schemas import UserRegistrationRequest, UserRegistrationResponse
 
+# initialize FastAPI
 app = FastAPI()
 
 # CORS configuration
 origins = [
     "http://localhost",
-    "http://localhost:3000",  # Next.js app origin
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
@@ -29,20 +28,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
-
 # initialize Firebase Admin SDK
 firebase_cred = firebase_admin.credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(firebase_cred)
 
+# initialize firestore db
 db = firestore.client()
 
 # initialize OpenAI API
 openai_client = OpenAI(api_key=os.getenv("PERSONAL_TUTOR_OPENAI_API_KEY"))
 
-# routes
-app.include_router(tutors_router, prefix="/v1")
+# routes config
+app.include_router(messages_router, prefix="/v1")
 app.include_router(modules_router, prefix="/v1")
+app.include_router(tutors_router, prefix="/v1")
 
 # register user api
 @app.post("/v1/register", response_model=UserRegistrationResponse)
@@ -61,67 +60,3 @@ async def register_user(user_data: UserRegistrationRequest):
         return UserRegistrationResponse(user_id=user.uid)
     except exceptions.FirebaseError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
-# fetch threads api
-@app.get("/v1/threads")
-async def list_threads(order: str = Query("created_at"), limit: int = Query(20)):
-    try:
-        # threads = openai_client.beta.threads.list(
-        #     order=order,
-        #     limit=str(limit),
-        # )
-
-        doc_ref = db.collection("threads").limit(limit).order_by(order)
-        doc = doc_ref.get()
-
-        return doc
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-# create message api
-@app.post("/v1/messages", response_model=MessageResponse)
-async def create_message(user_data: MessageRequest):
-    try:
-        thread_id = user_data.thread_id
-        instructions = user_data.instructions if user_data.instructions else "Please address the user as {user_data.firstName}. The user is a {user_data.userType}"
-
-        # create message
-        openai_client.beta.threads.messages.create(
-            thread_id=thread_id,
-            role="user",
-            content=user_data.content
-        )
-
-        # run assistant
-        openai_client.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=user_data.assistant_id,
-            instructions=instructions,
-        )
-
-        # assistant response
-        messages = openai_client.beta.threads.messages.list(
-            thread_id=thread_id
-        )
-
-        message_data = [message.model_dump() for message in messages.data]
-
-        return MessageResponse(object=messages.object, data=message_data)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-# fetch messages api
-@app.get("/v1/messages", response_model=MessageResponse)
-async def list_messages(thread_id: str):
-    try:
-        messages = openai_client.beta.threads.messages.list(
-            thread_id=thread_id
-        )
-
-        # convert message instances to dictionaries
-        message_data = [message.model_dump() for message in messages.data]
-
-        return MessageResponse(object=messages.object, data=message_data)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
--- schemas.py
+++ schemas.py
@@ -1,5 +1,3 @@
-from typing import Dict, List, Optional, Union
-
 from pydantic import BaseModel
 from pydantic_extra_types.isbn import *
 
@@ -23,29 +21,3 @@ class UserLoginResponse(BaseModel):
     auth_time: str
     user_id: str
     email_verified: bool
-
-# openai schemas
-class MessageRequest(BaseModel):
-    assistant_id: str
-    thread_id: str
-    content: str
-    instructions: Optional[str]
-    user_id: str
-    firstName: str
-    userType: str
-
-class TextContent(BaseModel):
-    type: str
-    text: Dict[str, Union[str, List[str]]]
-
-class Message(BaseModel):
-    created_at: int
-    id: str
-    object: str
-    thread_id: str
-    role: str
-    content: List[TextContent]
-
-class MessageResponse(BaseModel):
-    object: str
-    data: List[Message]
